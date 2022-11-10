c3cb7e39d091995298bc2ec4b8d028fa4f5d187e
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2022-11-11 00:06:01 +0100

    class 4

--- src/Pages/SingleProduct.js
+++ src/Pages/SingleProduct.js
@@ -1,9 +1,29 @@
 import React from "react";
-
+import '../Styles/SingleProduct.css'
+import Foto1 from "../Assets/Foto-1.png"
 
 const SingleProduct = () => {
     return (
-        <h1>SingleProduct!</h1>
+        <div>
+            <h5 className="productTag">Fable of Klassik - Jacket</h5>
+
+            <div className="sinProductCon">
+                <div className="sinProductImgCon">
+                    <img src={Foto1} alt='single product image' />
+                </div>
+                <div className="sinProductDesCon">
+                    <h1 className="sinProductHeading">JAСKET KLS</h1>
+                    <h2 className="sinProductPrice">$105</h2>
+                    <button className="sinProductButton">Add to cart</button>
+
+                    <div>
+                        <h4 className="sinProductDesHeading">Product Info</h4>
+                        <p className="sinProductDes">Jacket made of a loose fit makes the product a universal element of the upper layer. Two patch pockets and one hidden pocket. Branded lining with FABLE pattern. Shoulder pads of medium rigidity for shaping.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
